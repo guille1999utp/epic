2e2aacd28fe4af4f3afea520273b80f22cf132e1
guille1999utp <90706729+guille1999utp@users.noreply.github.com>
2024-07-31 01:59:43 -0500

    agregando migraciones de base de datos con alembic

---
+++ core/config.py
@@ -0,0 +1,4 @@
+class Settings:
+    SQLALCHEMY_DATABASE_URL: str = "sqlite:///./test.db"
+
+settings = Settings()
---
+++ models/__init__.py
@@ -0,0 +1,3 @@
+from models.reservation import Reservation
+from models.room import Room
+from database.main import Base
